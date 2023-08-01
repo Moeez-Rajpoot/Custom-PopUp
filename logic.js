cf0dc57c416c6f10060d1adcc6be55578becf96c
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2023-08-01 11:53:34 +0500

    Updated

--- logic.js
+++ logic.js
@@ -2,18 +2,12 @@ const pop = document.getElementById('pop');
 
 function openpop(params) {
 
-    pop.classList.remove('close-pop');
     pop.classList.add('open-pop');
+
     
 }
 
 function closepop(params) {
 
     pop.classList.remove("open-pop");
-    pop.classList.add("close-pop");
-    // After a short delay, reset the popup to its initial state
-    setTimeout(() => {
-        pop.classList.remove("close-pop");
-    }, 400);
-    
 }
